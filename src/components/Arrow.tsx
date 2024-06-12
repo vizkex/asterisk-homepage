06cac3e9b057f09bdb044ce50addb5ff0f78c19c
vizkex <vizkex@gmail.com>
2024-06-12 13:00:28 +0300

    Adding links to the header

---
+++ src/components/Arrow.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Arrow: React.FC = () => {
+  return (
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.47491 7.82495V4.52512M9.47491 4.52512H6.17508M9.47491 4.52512L4.52516 9.47487" stroke="#5D5D5D" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  );
+};
+
+export default Arrow;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Arrow from './Arrow';
 
 const About: React.FC = () => {
   return (
     <header className='mainheader'>
       <h1>Astersik<span>*</span></h1>
       <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <Link to="/">Home <Arrow /> </Link>
+      <Link to="/about">About <Arrow /> </Link>
+      <Link to="/pricing">Pricing <Arrow /> </Link>
+      <Link to="/about">Showcase <Arrow /> </Link>
       </nav>
     </header>
   );
